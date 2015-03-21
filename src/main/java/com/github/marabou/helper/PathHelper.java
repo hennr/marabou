069a63e9b412a2d0ea25a79d20eb6c8fd03a4e05
Jan-Hendrik Peters <hennr@hennr.name>
2015-03-21 19:36:39 +0100

    simplify pathHelper and honor the XDG Base Directory Specification under unix

--- src/main/java/com/github/marabou/helper/PathHelper.java
+++ src/main/java/com/github/marabou/helper/PathHelper.java
@@ -23,61 +23,39 @@ package com.github.marabou.helper;
 
 import java.io.File;
 
-/**
- * this class is intended to find the correct path for files under several
- * operating systems
- */
 public class PathHelper {
 
-	private boolean isUnix;
-	private boolean isWindows;
-	private boolean isMacOS;
-	/** contains ending slash under Unix or backslash under Windows */
-	private String userHome;
     final String PROPERTIES_FILE_NAME = "marabou.properties";
 
-    /**
-	 * detects and remembers the OS marabou is running on
-	 */
-	public PathHelper() {
-		// Unix
-		if (System.getProperty("os.name").toLowerCase().contains("nix")
-				|| System.getProperty("os.name").toLowerCase().contains("nux")
-				|| System.getProperty("os.name").toLowerCase().contains("bsd")) {
-			isUnix = true;
-			userHome = System.getProperty("user.home") + "/";
-		// Windows
-		} else if (System.getProperty("os.name").toLowerCase().contains("win")) {
-			isWindows = true;
-			userHome = System.getProperty("user.home") + "\\";
-		// MacOS
-		} else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
-			isMacOS = true;
-			userHome = System.getProperty("user.home") + File.separator;
-		}
-	}
+    public String getUserPropertiesFilePath() {
+        return getMarabouHomeFolder() + PROPERTIES_FILE_NAME;
+    }
 
-	/**
-	 * returns the path to marabou's config folder in the users home
-	 * including "/" at the end of the path
-	 * Does not check if file exists!
-	 */
-	public String getMarabouHomeFolder() {
-		// Unix
-		if (isUnix) {
-			return userHome + ".marabou/";
-		} else if (isWindows){
+	private String getMarabouHomeFolder() {
+		if (isUnix()) {
+			return System.getProperty("user.home") + File.separator + ".config" + File.separator + "marabou" + File.separator;
+		} else if (isWindows()) {
 			return System.getProperty("user.home") + File.separator + "marabou" + File.separator;
-		} else if (isMacOS) {
-			// TODO macos
-			return "";
+		} else if (isMacOS()) {
+            return System.getProperty("user.home") + File.separator + ".marabou" + File.separator;
 		} else {
 			throw new RuntimeException("Your operating system couldn't get detected properly. Please file a bug report.");
 		}
 	}
 
-    public String getUserPropertiesFilePath() {
-        return getMarabouHomeFolder() + PROPERTIES_FILE_NAME;
+    private boolean isUnix() {
+        return
+                System.getProperty("os.name").toLowerCase().contains("nix") ||
+                System.getProperty("os.name").toLowerCase().contains("nux") ||
+                System.getProperty("os.name").toLowerCase().contains("bsd");
+    }
+
+    private boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
+    private boolean isMacOS() {
+        return System.getProperty("os.name").toLowerCase().contains("mac");
     }
 
     public String getUserPropertiesFileName() {
