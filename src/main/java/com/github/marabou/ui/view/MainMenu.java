627474b4e8dcc3dcca677c2219ce95278f56a58a
Jan-Hendrik Peters <hennr@hennr.name>
2015-08-25 22:30:05 +0200

    java 8 goodness and reformatting

--- src/main/java/com/github/marabou/ui/view/MainMenu.java
+++ src/main/java/com/github/marabou/ui/view/MainMenu.java
@@ -35,7 +35,7 @@ import org.eclipse.swt.widgets.*;
  */
 public class MainMenu extends BaseGuiClass {
     private final ImageLoader imageLoader;
-	private Menu menu;
+    private Menu menu;
     private MainMenuController mainMenuController;
 
 
@@ -43,90 +43,70 @@ public class MainMenu extends BaseGuiClass {
         this.mainMenuController = mainMenuController;
         this.menu = new Menu(shell, SWT.BAR);
         this.imageLoader = new ImageLoader();
-	}
+    }
 
-	public Menu getMenu() {
-		return menu;
-	}
+    public Menu getMenu() {
+        return menu;
+    }
 
-	public void init() {
-		// File
-		MenuItem file = new MenuItem(menu, SWT.CASCADE);
-		file.setText(_("&File"));
+    public void init() {
+        // File
+        MenuItem file = new MenuItem(menu, SWT.CASCADE);
+        file.setText(_("&File"));
 
-		Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
-		file.setMenu(fileMenu);
+        Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+        file.setMenu(fileMenu);
 
-		// File -> Open File
-		MenuItem openFileItem = new MenuItem(fileMenu, SWT.PUSH);
-		openFileItem.setText(_("Open &file\t Ctrl+F"));
+        // File -> Open File
+        MenuItem openFileItem = new MenuItem(fileMenu, SWT.PUSH);
+        openFileItem.setText(_("Open &file\t Ctrl+F"));
 
-		openFileItem.setAccelerator(SWT.CTRL + 'F');
-		openFileItem.setImage(imageLoader.getImage(AvailableImage.AUDIO_ICON));
+        openFileItem.setAccelerator(SWT.CTRL + 'F');
+        openFileItem.setImage(imageLoader.getImage(AvailableImage.AUDIO_ICON));
 
-		// listener for File -> open file
-		openFileItem.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(final Event e) {
-                mainMenuController.handleOpenFileEvent();
-            }
-		});
+        // listener for File -> open file
+        openFileItem.addListener(SWT.Selection, event -> mainMenuController.handleOpenFileEvent());
 
-		// File -> open directory
-		MenuItem openDirectoryItem = new MenuItem(fileMenu, SWT.PUSH);
-		openDirectoryItem.setText(_("Open &directory\t Ctrl+D"));
-		openDirectoryItem.setAccelerator(SWT.CTRL + 'D');
+        // File -> open directory
+        MenuItem openDirectoryItem = new MenuItem(fileMenu, SWT.PUSH);
+        openDirectoryItem.setText(_("Open &directory\t Ctrl+D"));
+        openDirectoryItem.setAccelerator(SWT.CTRL + 'D');
 
-		openDirectoryItem.setImage(imageLoader.getImage(AvailableImage.FOLDER_ICON));
+        openDirectoryItem.setImage(imageLoader.getImage(AvailableImage.FOLDER_ICON));
 
-		// listener for File -> open directory
-		openDirectoryItem.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event e) {
-                mainMenuController.handleOpenDirectoryEvent();
-				}
-		});
+        // listener for File -> open directory
+        openDirectoryItem.addListener(SWT.Selection, event -> mainMenuController.handleOpenDirectoryEvent());
 
-		// File -> save current file
-		MenuItem saveItem = new MenuItem(fileMenu, SWT.PUSH);
-		saveItem.setText(_("&Save\t Ctrl+S"));
-		saveItem.setAccelerator(SWT.CTRL + 'S');
-		saveItem.setImage(imageLoader.getImage(AvailableImage.SAVE_ICON));
+        // File -> save current file
+        MenuItem saveItem = new MenuItem(fileMenu, SWT.PUSH);
+        saveItem.setText(_("&Save\t Ctrl+S"));
+        saveItem.setAccelerator(SWT.CTRL + 'S');
+        saveItem.setImage(imageLoader.getImage(AvailableImage.SAVE_ICON));
 
-		saveItem.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event e) {
-					mainMenuController.handleSaveSelectedFilesEvent();
-			}
-		});
+        saveItem.addListener(SWT.Selection, event -> mainMenuController.handleSaveSelectedFilesEvent());
 
-		// File -> Exit
-		MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
-		exitItem.setImage(imageLoader.getImage(AvailableImage.EXIT_ICON));
-		exitItem.setText(_("&Exit\t Alt+F4"));
+        // File -> Exit
+        MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
+        exitItem.setImage(imageLoader.getImage(AvailableImage.EXIT_ICON));
+        exitItem.setText(_("&Exit\t Alt+F4"));
 
-		// listener for File -> Exit
-		exitItem.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(final Event e) {
-				mainMenuController.handleExitEvent(shell);
-			}
-		});
+        // listener for File -> Exit
+        exitItem.addListener(SWT.Selection, event -> mainMenuController.handleExitEvent(shell));
 
-		// Help
-		MenuItem help = new MenuItem(menu, SWT.CASCADE);
-		help.setText(_("&Help"));
+        // Help
+        MenuItem help = new MenuItem(menu, SWT.CASCADE);
+        help.setText(_("&Help"));
 
-		Menu helpMenu = new Menu(shell, SWT.DROP_DOWN);
-		help.setMenu(helpMenu);
+        Menu helpMenu = new Menu(shell, SWT.DROP_DOWN);
+        help.setMenu(helpMenu);
 
-		MenuItem aboutItem = new MenuItem(helpMenu, SWT.PUSH);
-		aboutItem.setText(_("&About\t F1"));
-		aboutItem.setAccelerator(SWT.F1);
+        MenuItem aboutItem = new MenuItem(helpMenu, SWT.PUSH);
+        aboutItem.setText(_("&About\t F1"));
+        aboutItem.setAccelerator(SWT.F1);
 
-		aboutItem.setImage(imageLoader.getImage(AvailableImage.HELP_ICON));
+        aboutItem.setImage(imageLoader.getImage(AvailableImage.HELP_ICON));
 
-		// listener for Help -> About
-		aboutItem.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(final Event e) {
-                mainMenuController.handleShowAboutWindow();
-			}
-		});
-	}
+        // listener for Help -> About
+        aboutItem.addListener(SWT.Selection, event -> mainMenuController.handleShowAboutWindow());
+    }
 }
